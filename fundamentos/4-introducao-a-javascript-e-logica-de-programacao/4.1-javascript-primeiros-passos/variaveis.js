7e626866dedfa1ca241254196976b99d823be409
Rafael Soares <r.soares2015@outlook.com>
2022-06-21 16:26:15 -0300

    Primeiros exercicios de JavaScript

---
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.1-javascript-primeiros-passos/variaveis.js
@@ -0,0 +1,13 @@
+const myName = "Rafael";
+const birthCity = "São Paulo";
+let birthYear = 1996;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+console.log(birthYear);
+
+birthCity = Maranello;
+console.log(birthCity);
