d72788e09d91d32750245de8f1063d5ba3aa58ce
Rafael Soares <r.soares2015@outlook.com>
2022-06-22 16:20:28 -0300

    Exercicio sobre for-of resolvido

---
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.2-javascript-array-e-loop-for/for-of/for-of.js
@@ -0,0 +1,7 @@
+let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+
+//Utilize o for/of para imprimir os elementos da lista names com o console.log():
+
+for (let nomes of names) {
+    console.log(nomes);
+}
