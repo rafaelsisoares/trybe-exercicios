815c1906eddf987b1f401970fe28952b7a558442
Rafael Soares <r.soares2015@outlook.com>
2022-06-22 22:45:03 -0300

    Pequena correção no comentário

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.2-javascript-array-e-loop-for/exercicios/bonus.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.2-javascript-array-e-loop-for/exercicios/bonus.js
@@ -28,6 +28,12 @@ for (let index = 1; index < numbers.length; index += 1) {
 
 console.log(numbers);
 
+/* Agora crie um novo array a partir do array numbers, sem perdê-lo.
+Cada valor do novo array deverá ser igual ao valor correspondente no array numbers multiplicado pelo seguinte.
+Por exemplo: o primeiro valor do novo array deverá ser 45, pois é a multiplicação de 5 (primeiro valor) e 9 (valor seguinte). 
+Já o segundo valor do novo array deverá ser 27, pois é a multiplicação de 9 (segundo valor) e 3 (valor seguinte), e assim por diante. 
+Caso não haja próximo valor, a multiplicação deverá ser feita por 2. Faça isso utilizando o for e o método push. */
+
 numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let numbersMulti = [];
 for (let i = 0; i < numbers.length; i += 1) {
