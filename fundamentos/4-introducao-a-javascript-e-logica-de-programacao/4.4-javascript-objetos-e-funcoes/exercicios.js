c58e3c3335b0d40d4a1d039fd5b3ee9d1c84ff33
Rafael Soares <r.soares2015@outlook.com>
2022-06-24 18:36:58 -0300

    Quinto exercicio resolvido

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/exercicios.js
@@ -2,7 +2,7 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+};
 
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
 //Valor esperado no console: "Bem-vinda: Margarida"
@@ -13,3 +13,34 @@ console.log('Bem-vinda:', info.personagem);
 
 info.recorrente = 'Sim';
 console.log(info);
+
+//3 - Faça um for/in que mostre todas as chaves do objeto.
+
+for (let key in info) {
+    console.log(key);
+}
+
+//4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto.
+
+for (let key in info) {
+    console.log(info[key]);
+}
+
+/* 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 
+'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. 
+Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. */
+
+let infoPatinhas = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: 'Sim'
+};
+
+for (let key in info) {
+    if (info[key] === infoPatinhas[key]) {
+        console.log('Ambos recorrentes');
+    } else {
+        console.log(info[key] + ' e ' + infoPatinhas[key]);
+    }
+}
