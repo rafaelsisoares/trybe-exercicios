e11a92de55d23fd79395ed66e663108877d3b036
Rafael Soares <r.soares2015@outlook.com>
2022-06-24 18:50:05 -0300

    Setimo exercicio resolvido

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/exercicios.js
@@ -61,3 +61,8 @@ let leitor = {
 //6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log.
 
 console.log("O livro favorito de " + leitor.nome + " " + leitor.sobrenome + " se chama " + leitor.livrosFavoritos[0].titulo );
+
+//7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array.
+
+leitor.livrosFavoritos.push({titulo: 'Harry Potter e o Prisioneiro de Azkaban', autor: 'JK Rowling', editora: 'Rocco'});
+
