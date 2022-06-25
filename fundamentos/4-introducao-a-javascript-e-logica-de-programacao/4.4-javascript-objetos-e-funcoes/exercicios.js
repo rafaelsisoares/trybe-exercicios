1926132b2da4e6a8627a9ed1a663a8f22abded01
Rafael Soares <r.soares2015@outlook.com>
2022-06-24 21:48:17 -0300

    Quarto exercicio sobre funções resolvido

--- fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/4-introducao-a-javascript-e-logica-de-programacao/4.4-javascript-objetos-e-funcoes/exercicios.js
@@ -89,3 +89,57 @@ function palindromo(word) {
 }
 
 console.log(palindromo('arara'));
+
+//2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+
+let sequenciaNumerica = [2, 3, 6, 7, 10, 1];
+
+function indiceDoMaior(array) {
+    let indexMax = 0;
+    for (let key = 0; key < array.length; key += 1) {
+        if (array[indexMax] < array[key]) {
+            indexMax = key;
+        }
+    }
+    return indexMax;    
+}
+
+console.log(indiceDoMaior(sequenciaNumerica));
+
+//3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+
+let conjuntoNumerico = [2, 4, 6, 7, 10, 0, -3];
+function indiceDoMenor(array) {
+    let indexMin = 0;
+    for (let i = 0; i < array.length; i += 1) {
+        if (array[indexMin] > array[i]) {
+            indexMin = i;
+        }
+    }
+    return indexMin;
+}
+
+console.log(indiceDoMenor(conjuntoNumerico));
+//4 - Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+
+let nomes = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+
+function maiorNome(array) {
+    let nomeMaior = '';
+    for (let key in array) {
+        if (array[key].length > nomeMaior.length) {
+            nomeMaior = array[key];
+        }
+    }
+    return nomeMaior;
+}
+
+console.log(maiorNome(nomes));
+
+// 5 - Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+
+//6 - Crie uma função que receba um número natural (número inteiro não negativo) N e retorne o somatório de todos os números de 1 até N.
+
+
+//7 - Crie uma função que receba uma string word e outra string ending. Verifique se a string ending é o final da string word. 
+//Considere que a string ending sempre será menor que a string word.
