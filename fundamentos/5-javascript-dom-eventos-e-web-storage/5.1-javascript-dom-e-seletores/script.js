69fe58ab67074683ced81321192b71d7e2028a88
Rafael Soares <r.soares2015@outlook.com>
2022-06-28 22:43:14 -0300

    Resolução dos exercicios de fixação

--- fundamentos/5-javascript-dom-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
+++ fundamentos/5-javascript-dom-eventos-e-web-storage/5.1-javascript-dom-e-seletores/script.js
@@ -4,23 +4,22 @@ const emergency = document.getElementsByClassName('emergency-tasks');
 const nonEmergency = document.getElementsByClassName('no-emergency-tasks');
 const emergencyTitle = document.querySelectorAll('.emergency-tasks div h3');
 const noEmergencyTitle = document.querySelectorAll(".no-emergency-tasks div h3");
-const rodape = document.querySelector('#footer div');
+const rodape = document.getElementById('footer-container');
 
-cabecalho.innerText = 'Administrador do Tempo da Trybe';
 cabecalho.style.backgroundColor = '#00b069';
-cabecalho.style.fontSize = '25px';
 
 corpo.style.backgroundColor = '#f3f3f3';
 
 emergency[0].style.backgroundColor = '#ff9f84';
 nonEmergency[0].style.backgroundColor = '#f9db5e';
 
-for (let i in emergencyTitle) {
-    emergencyTitle[i].style.backgroundColor = '#a500f3';
+
+for (let j of noEmergencyTitle) {
+    j.style.backgroundColor = '#232525';
 }
 
-for (let j in noEmergencyTitle) {
-    noEmergencyTitle[j].style.backgroundColor = 'blue';
+for (let i of emergencyTitle) {
+    i.style.backgroundColor = '#a500f3';
 }
 
-rodape.style,backgroundColor = '#003533';
+rodape.style.backgroundColor = '#003533';
