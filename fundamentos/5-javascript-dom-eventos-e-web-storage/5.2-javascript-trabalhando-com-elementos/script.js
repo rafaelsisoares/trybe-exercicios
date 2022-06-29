e76d211aca0bc753b5ad1908e39c40cd775a732e
Rafael Soares <r.soares2015@outlook.com>
2022-06-29 18:32:50 -0300

    Resolução do primeiro exercicio

--- fundamentos/5-javascript-dom-eventos-e-web-storage/5.2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/5-javascript-dom-eventos-e-web-storage/5.2-javascript-trabalhando-com-elementos/script.js
@@ -6,6 +6,12 @@ const brother = atual.previousElementSibling;
 const texto = atual.nextSibling;
 const terceiroFilho = atual.nextElementSibling;
 const thirdChild = pai.lastElementChild.previousElementSibling;
+const brother2 = document.createElement('section');
+const filhoAtual = document.createElement('aside');
+const filhoDoFilhoDoFilho = document.createElement('div');
+const terceiroFilho2 = filhoDoFilhoDoFilho.parentNode;
 
 pai.style.color = 'rgb(50, 100, 100)';
 filhoDoFilho.innerText = 'Texto adicionado por JavaScript';
+atual.appendChild(filhoAtual)
+filhoDoFilho.appendChild(filhoDoFilhoDoFilho);
