b8d39399b5b3dec3c9160ba4128bbac5d8752c29
Rafael Soares <r.soares2015@outlook.com>
2022-07-01 22:02:04 -0300

    Resolução do segundo exercicio

--- fundamentos/5-javascript-dom-eventos-e-web-storage/5.4-javascript-web-storage/exercicio/script.js
+++ fundamentos/5-javascript-dom-eventos-e-web-storage/5.4-javascript-web-storage/exercicio/script.js
@@ -1,6 +1,6 @@
 const botaoDark = document.getElementById('dark');
 const botaoCor = document.getElementById('cor');
-/* const botao */
+const botaoTamanho = document.getElementById('font-size');
 
 function darkMode() {
     let darkClass = document.getElementsByClassName('dark');
@@ -20,12 +20,32 @@ function darkMode() {
     ;
 }
 
+function fontSize() {
+    let sizeClass = document.getElementsByClassName('sizeOfFont');
+    let main = document.getElementById('tamanhoDaFonte');
+    if(sizeClass.length === 0 || localStorage.getItem('size') === 'false') {
+        main.classList = 'sizeOfFont';
+        let troca = localStorage.getItem('size');
+            troca = 'true';
+            localStorage.setItem('size', troca);
+    } else {
+        main.removeAttribute('class');
+        let troca = localStorage.getItem('size');
+            troca = 'false';
+            localStorage.setItem('size', troca);
+    }
+}
+
 botaoDark.addEventListener('click', darkMode);
+botaoTamanho.addEventListener('click', fontSize);
 
 window.onload = function() {
     if(localStorage.length > 0) {
         if(localStorage.getItem('dark') === 'true') {
             darkMode();
+        }
+        if(localStorage.getItem('size') === 'true'){
+            fontSize();
         }
     }
 }
