c021ad7ae8e72f3aafde97729f4dd7f3b5162e88
Rafael Soares <r.soares2015@outlook.com>
2022-07-14 19:08:26 -0300

    1 e 2

---
+++ fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/7.1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -0,0 +1,23 @@
+//1  Altere as variáveis para respeitarem o escopo em que foram declaradas.
+
+function testingScope(escopo) {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+    //console.log(`${ifScope} o que estou fazendo aqui ? :O`); // Se necessário esta linha pode ser removida.
+  }
+
+  testingScope(false);
+  //console.log(elseScope); -> essa linha apresentará um erro afirmando que a variável elseScope não foi declarada
+
+//2 Crie uma função que retorne um array em ordem crescente.
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const sortArray = (array) => array.sort((a, b) => a - b);
+
+console.log(sortArray(oddsAndEvens)); // será necessário alterar essa linha 😉
