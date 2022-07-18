4fa5e836c411e1406d22d1fc4276ee8f09954b29
Rafael Soares <r.soares2015@outlook.com>
2022-07-18 18:47:21 -0300

    Segundo teste feito

--- fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/7.3-primeiros-passos-em/sum.test.js
+++ fundamentos/7-introducao-a-javascript-es6-e-testes-unitarios/7.3-primeiros-passos-em/sum.test.js
@@ -4,4 +4,7 @@ describe('Testes da função sum', () => {
     it('Testa se ao receber os parâmetros 4 e 5, a função retorna 9', () => {
         expect(sum(4, 5)).toBe(9);
     })
+    it('Testa se ao receber os parâmetros 0 e 0, a função retorna 0', () => {
+        expect(sum(0, 0)).toBe(0);
+    })
 });
