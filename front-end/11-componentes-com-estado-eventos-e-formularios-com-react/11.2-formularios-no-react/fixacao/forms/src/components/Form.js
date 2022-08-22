7020dde6f1741145eea7a36d717955ecec672985
Rafael Soares <r.soares2015@outlook.com>
2022-08-22 18:35:31 -0300

    Terceira parte dos exercícios de fixação sobre formulários em React

--- front-end/11-componentes-com-estado-eventos-e-formularios-com-react/11.2-formularios-no-react/fixacao/forms/src/components/Form.js
+++ front-end/11-componentes-com-estado-eventos-e-formularios-com-react/11.2-formularios-no-react/fixacao/forms/src/components/Form.js
@@ -21,25 +21,32 @@ class Form extends React.Component{
             <div>
                 <h1>Formulário em React</h1>
                 <form>
-                    <label> Escolha uma equipe:
-                        <select name="team" onChange={this.handleChange}>
-                            <option value="">Escolha uma opção...</option>
-                            <option value="mercedes">Mercedes</option>
-                            <option value="rbr">RedBull</option>
-                            <option value="sBinnala">Ferrari</option>
-                        </select>
-                    </label>
+                    <fieldset>
+                        <label> Escolha uma equipe:
+                            <select name="team" onChange={this.handleChange}>
+                                <option value="">Escolha uma opção...</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="rbr">RedBull</option>
+                                <option value="sBinnala">Ferrari</option>
+                            </select>
+                        </label>
+                    </fieldset>
                     <label>Quem ganhará o Mundial de Pilotos deste ano?
                         <input type="text" name="champion" onChange={this.handleChange} />
                     </label>
                     <label>Quantas corridas você acha que o calendário deveria ter?
                         <input type="number" name="races" onChange={this.handleChange} />
                     </label>
-                    <label>Descreva o que é a F1 para você
-                        <textarea name="opinion" onChange={this.handleChange} />
-                    </label>
+                    <fieldset>
+                        <label>Descreva o que é a F1 para você
+                            <textarea name="opinion" onChange={this.handleChange} />
+                        </label>
+                    </fieldset>
                     <label>Autorizo o compartilhamento dos dados inseridos.
                         <input type="checkbox" name="agreement" onChange={this.handleChange}/>
+                    </label>
+                    <label>Upload
+                        <input type="file" name="upload" />
                     </label>
                 </form> 
             </div>
