a7e865146839dbe5f506274705e30aecf2a6907b
Rafael Soares <r.soares2015@outlook.com>
2022-09-16 00:16:41 -0300

    Exercícios de fixação sobre mock finalizados

--- front-end/14-testes-automatizados-com-react-testing-library/14.2-rtl-mocks-e-inputs/fixacao/mocks/src/App.test.js
+++ front-end/14-testes-automatizados-com-react-testing-library/14.2-rtl-mocks-e-inputs/fixacao/mocks/src/App.test.js
@@ -4,14 +4,34 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import userEvent from "@testing-library/user-event";
 
+const joke = {
+  id: "7h3oGtrOfxc",
+  joke: "Whiteboards ... are remarkable.",
+  status: 200,
+};
+
+const newJoke = {
+  id: "R7UfaahVfFd",
+  joke: "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
+  status: 200,
+};
+
 afterEach(() => jest.clearAllMocks());
 
+beforeEach(() => {
+  jest.spyOn(global, "fetch");
+  global.fetch.mockResolvedValueOnce({
+    json: jest.fn().mockResolvedValueOnce(joke),
+  });
+
+  jest.spyOn(global, "fetch");
+  global.fetch.mockResolvedValue({
+    json: jest.fn().mockResolvedValue(newJoke),
+  });
+
+})
+
 it("fetches a joke", async () => {
-  const joke = {
-    id: "7h3oGtrOfxc",
-    joke: "Whiteboards ... are remarkable.",
-    status: 200,
-  };
 
   jest.spyOn(global, "fetch");
   global.fetch.mockResolvedValue({
@@ -30,23 +50,6 @@ it("fetches a joke", async () => {
 });
 
 it("fetches a new joke", async () => {
-  const joke = {
-    id: "7h3oGtrOfxc",
-    joke: "Whiteboards ... are remarkable.",
-    status: 200,
-  };
-
-  jest.spyOn(global, "fetch");
-  global.fetch.mockResolvedValue({
-    json: jest.fn().mockResolvedValue(joke),
-  });
-
-  const newJoke = {
-    id: "R7UfaahVfFd",
-    joke: "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away.",
-    status: 200,
-  };
-
   render(<App />);
   const btnNewJoke = screen.getByRole("button");
   const renderedJoke = await screen.findByText(
@@ -56,18 +59,12 @@ it("fetches a new joke", async () => {
   expect(renderedJoke).toBeInTheDocument();
   
   userEvent.click(btnNewJoke);
-
-  jest.spyOn(global, "fetch");
-  global.fetch.mockResolvedValue({
-    json: jest.fn().mockResolvedValue(newJoke),
-  });
-
   const renderedNewJoke = await screen.findByText(
     "My dog used to chase people on a bike a lot. It got so bad I had to take his bike away."
   );
 
   expect(renderedNewJoke).toBeInTheDocument();
-  expect(renderedJoke).not.toBeInTheDocument();
+  expect(screen.queryByText(joke.joke)).not.toBeInTheDocument();
   expect(global.fetch).toBeCalledTimes(2);
   expect(global.fetch).toBeCalledWith("https://icanhazdadjoke.com/", {
     headers: { Accept: "application/json" },
