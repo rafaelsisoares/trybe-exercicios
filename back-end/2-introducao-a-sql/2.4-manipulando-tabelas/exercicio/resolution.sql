fa768f594022de0c946d73e316e6b1f1e80f0f4f
Rafael Soares <r.soares2015@outlook.com>
2022-11-21 22:11:14 -0300

    feat: 1° exercício

---
+++ back-end/2-introducao-a-sql/2.4-manipulando-tabelas/exercicio/resolution.sql
@@ -0,0 +1,7 @@
+USE pixar;
+-- 1.
+INSERT INTO movies (title, director, year, length_minutes) VALUES
+('Monstros SA', 'Pete Docter', 2001, 92),
+('Procurando Nemo', 'John Lasseter', 2003, 107),
+('Os Incriveis', 'Brad Bird', 2004, 116),
+('WALL-E', 'Pete Docter', 2008, 104);
