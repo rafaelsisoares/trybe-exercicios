58c8e856614d950072daee8ee970d6e7be03dab7
Rafael Soares <r.soares2015@outlook.com>
2022-11-21 22:12:25 -0300

    feat: 2° exercício

--- back-end/2-introducao-a-sql/2.4-manipulando-tabelas/exercicio/resolution.sql
+++ back-end/2-introducao-a-sql/2.4-manipulando-tabelas/exercicio/resolution.sql
@@ -5,3 +5,7 @@ INSERT INTO movies (title, director, year, length_minutes) VALUES
 ('Procurando Nemo', 'John Lasseter', 2003, 107),
 ('Os Incriveis', 'Brad Bird', 2004, 116),
 ('WALL-E', 'Pete Docter', 2008, 104);
+
+-- 2.
+INSERT INTO box_office (movie_id, rating, domestic_sales, international_sales)
+VALUES (9, 6.8, 450000000, 370000000);
