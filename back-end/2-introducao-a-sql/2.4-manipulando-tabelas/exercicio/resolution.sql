4846497f65fc348ca1761f4866887f07cfdd81c6
Rafael Soares <r.soares2015@outlook.com>
2022-11-21 22:55:48 -0300

    feat: 9° exercício

--- back-end/2-introducao-a-sql/2.4-manipulando-tabelas/exercicio/resolution.sql
+++ back-end/2-introducao-a-sql/2.4-manipulando-tabelas/exercicio/resolution.sql
@@ -39,3 +39,8 @@ DELETE FROM movies WHERE director = 'Andrew Stanton';
 UPDATE box_office
 SET rating = 9.0
 WHERE domestic_sales > 400000000;
+
+-- 9.
+UPDATE box_office
+SET rating = 6.0
+WHERE international_sales < 300000000 AND domestic_sales > 200000000;
