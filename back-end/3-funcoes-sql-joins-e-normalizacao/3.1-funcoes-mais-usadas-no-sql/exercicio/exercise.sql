1f1f902d3bf0385301cf49153ba2a8a1816ef57f
Rafael Soares <r.soares2015@outlook.com>
2022-11-23 22:44:36 -0300

    feat: 9° exercício

--- back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
+++ back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
@@ -34,3 +34,9 @@ GROUP BY job_id;
 SELECT job_id, SUM(salary) AS total FROM hr.employees
 GROUP BY job_id
 HAVING job_id = 'it_prog';
+
+-- 9. Escreva uma query que exiba em ordem decrescente a média salarial de todos os cargos, exceto das pessoas programadoras.
+SELECT job_id, AVG(salary) AS average_salary FROM hr.employees
+GROUP BY job_id
+HAVING job_id <> 'it_prog'
+ORDER BY average_salary DESC;
