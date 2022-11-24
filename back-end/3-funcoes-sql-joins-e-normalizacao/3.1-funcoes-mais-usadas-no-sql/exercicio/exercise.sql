1a21825449f13f489356628074821b86e4e389b8
Rafael Soares <r.soares2015@outlook.com>
2022-11-23 23:06:13 -0300

    feat: 13° exercício

--- back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
+++ back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
@@ -54,3 +54,6 @@ WHERE phone_number LIKE '515%';
 -- 12. Escreva uma query que só exiba as informações dos funcionários cujo o primeiro nome tenha oito ou mais caracteres.
 SELECT * FROM hr.employees
 WHERE first_name LIKE '________%';
+
+-- 13. Escreva uma query que exiba as seguintes informações de cada funcionário: id, primeiro nome e ano no qual foi contratado (exiba somente o ano).
+SELECT employee_id, first_name, YEAR(hire_date) AS hire_year FROM hr.employees;
