b611119a520903f984e56faa89c2f7769ba53231
Rafael Soares <r.soares2015@outlook.com>
2022-11-23 22:38:12 -0300

    feat: 8° exercício

--- back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
+++ back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
@@ -29,3 +29,8 @@ GROUP BY job_id HAVING job_id = 'it_prog';
 -- 7. Escreva uma query que exiba a quantidade de dinheiro necessária para efetuar o pagamento de cada profissão.
 SELECT job_id, SUM(salary) AS total FROM hr.employees
 GROUP BY job_id;
+
+-- 8. Utilizando a query anterior, faça as alterações para que seja exibido somente a quantidade de dinheiro necessária para cobrir a folha de pagamento das pessoas programadoras.
+SELECT job_id, SUM(salary) AS total FROM hr.employees
+GROUP BY job_id
+HAVING job_id = 'it_prog';
