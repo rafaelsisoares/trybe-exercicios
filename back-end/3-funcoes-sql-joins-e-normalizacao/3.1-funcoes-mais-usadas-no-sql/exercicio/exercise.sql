b690812b862b953f8e222b2b53250182d12c5ed8
Rafael Soares <r.soares2015@outlook.com>
2022-11-23 22:09:40 -0300

    feat: 3° exercício

--- back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
+++ back-end/3-funcoes-sql-joins-e-normalizacao/3.1-funcoes-mais-usadas-no-sql/exercicio/exercise.sql
@@ -3,3 +3,8 @@ SELECT MAX(salary) FROM hr.employees;
 
 -- 2. Escreva uma query que exiba a diferença entre o maior e o menor salário.
 SELECT MAX(salary) - MIN(salary) FROM hr.employees;
+
+-- 3. Escreva uma query que exiba a média salarial de cada job_id, ordenando pela média salarial em ordem decrescente.
+SELECT job_id, AVG(salary) AS average_salary FROM hr.employees
+GROUP BY job_id
+ORDER BY average_salary DESC;
