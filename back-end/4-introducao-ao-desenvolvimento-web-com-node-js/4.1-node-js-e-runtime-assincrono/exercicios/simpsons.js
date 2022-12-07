ddf3cdfc1f3570ede835169ca1cdac778485af92
Rafael Soares <r.soares2015@outlook.com>
2022-12-06 23:45:37 -0300

    feat: Adicionada funcao que adiciona Nelson Muntz a simpsonFamily.json

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.1-node-js-e-runtime-assincrono/exercicios/simpsons.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.1-node-js-e-runtime-assincrono/exercicios/simpsons.js
@@ -1,20 +1,20 @@
 const fs = require('fs').promises;
 const readline = require('readline-sync');
 
-const read = async () => {
-    const simpsonsData = await fs.readFile('./simpsons.json', 'utf-8');
+const read = async (file) => {
+    const simpsonsData = await fs.readFile(`./${file}`, 'utf-8');
     const simpsonsList = JSON.parse(simpsonsData);
     return simpsonsList;
 };
 
 const getAllSimpsons = async () => {
-    const simpsonsList = await read();
+    const simpsonsList = await read('simpsons.json');
     const simpsonsNames = simpsonsList.map(({ id, name }) => `${id} - ${name}`);
     simpsonsNames.forEach((name) => console.log(name));
 }
 
 const getSimpsonById = async (id) => {
-    const simpsonsList = await read();
+    const simpsonsList = await read('simpsons.json');
     const simpson = simpsonsList.find((character) => +character.id === id);
     if (!simpson) {
         throw new Error('ID not found');
@@ -23,15 +23,25 @@ const getSimpsonById = async (id) => {
 };
 
 const removeSimpson = async () => {
-    const simpsonsList = await read();
+    const simpsonsList = await read('simpsons.json');
     const newSimpsonsList = simpsonsList.filter(({ id }) => +id !== 10 && +id !== 6);
     await fs.writeFile('./simpsons.json', JSON.stringify(newSimpsonsList));
 }
 
 const createSimpsonsFamily = async () => {
-    const simpsonsList = await read();
+    const simpsonsList = await read('simpsons.json');
     const simpsonsFamily = simpsonsList.filter(({ id }) => +id >= 1 && +id <= 4);
     await fs.writeFile('./simpsonFamily.json', JSON.stringify(simpsonsFamily));
+};
+
+const addMember = async () => {
+    const simpsonsList = await read('simpsonFamily.json');
+    const newMember = {
+        id: '5',
+        name: 'Nelson Muntz'
+    };
+    const newSimpsonsList = [...simpsonsList, newMember];
+    await fs.writeFile('./simpsonFamily.json', JSON.stringify(newSimpsonsList));
 };
 
 async function main() {
@@ -44,6 +54,7 @@ async function main() {
     };
     removeSimpson();
     createSimpsonsFamily();
+    addMember();
 }
 
 main();
