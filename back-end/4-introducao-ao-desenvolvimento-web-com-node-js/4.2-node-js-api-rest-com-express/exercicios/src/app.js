f9cc5678707d0ec0c92c382adcb94333d9617960
Rafael Soares <r.soares2015@outlook.com>
2022-12-07 20:09:04 -0300

    fix: Correcoes no codigo

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.2-node-js-api-rest-com-express/exercicios/src/app.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.2-node-js-api-rest-com-express/exercicios/src/app.js
@@ -1,13 +1,13 @@
-const path = require("path");
-const express = require("express");
-const fs = require("fs").promises;
+const path = require('path');
+const express = require('express');
+const fs = require('fs').promises;
 
 const app = express();
 
 const read = async () => {
-  const moviesPath = path.resolve(__dirname, "./movies.json");
+  const moviesPath = path.resolve(__dirname, './movies.json');
   try {
-    const movies = await fs.readFile(moviesPath, "utf-8");
+    const movies = await fs.readFile(moviesPath, 'utf-8');
     const moviesList = JSON.parse(movies);
     return moviesList;
   } catch (e) {
