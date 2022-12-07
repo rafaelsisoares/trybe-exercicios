59440d63543d1a6620b5c4486de42c9f4f24c77b
Rafael Soares <r.soares2015@outlook.com>
2022-12-06 22:27:59 -0300

    feat: Descrição do IMC usando encadeamento de if

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.1-node-js-e-runtime-assincrono/exercicios/index.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.1-node-js-e-runtime-assincrono/exercicios/index.js
@@ -4,7 +4,21 @@ const { bodyMassIndex } = require('./bmi');
 function main() {
     const weight = readline.questionFloat("What' your weight? ");
     const height = readline.questionFloat("What' your height? ");
-    console.log(bodyMassIndex(weight, height));
+    const result = bodyMassIndex(weight, height);
+    console.log(result);
+    if (result < 18.5) {
+        console.log('Abaixo do peso')
+    } else if (result >= 18.5 && result <= 24.9) {
+        console.log('Peso normal');
+    } else if (result >= 25 && result <= 29.9) {
+        console.log('Acima do peso');
+    } else if (result >= 30 && result <= 34.9) {
+        console.log('Obesidade grau I');
+    } else if (result >= 35 && result <= 39.9) {
+        console.log('Obesidade grau II');
+    } else {
+        console.log('Obesidade graus III e IV');
+    }
 };
 
 main();
