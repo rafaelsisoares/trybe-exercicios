efd4be808b0fee2ac31930e2a97683bf0e6376a6
Rafael Soares <r.soares2015@outlook.com>
2022-12-06 21:55:22 -0300

    fix: Consertando o question do weight

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.1-node-js-e-runtime-assincrono/exercicios/index.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.1-node-js-e-runtime-assincrono/exercicios/index.js
@@ -2,7 +2,7 @@ const readline = require('readline-sync');
 const { bodyMassIndex } = require('./bmi');
 
 function main() {
-    const weight = readline.question("What' your weight? ");
+    const weight = readline.questionFloat("What' your weight? ");
     const height = readline.question("What' your height? ");
     console.log(bodyMassIndex(weight, height));
 };
