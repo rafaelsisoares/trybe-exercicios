cb5cf7ffe5ba80cfa51f09544d040f68c24e9524
Rafael Soares <r.soares2015@outlook.com>
2022-12-07 20:03:05 -0300

    feat: Adicionada funcao que faz a leitura do movies.json

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.2-node-js-api-rest-com-express/exercicios/src/app.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.2-node-js-api-rest-com-express/exercicios/src/app.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const path = require("path");
+const express = require("express");
+const fs = require("fs").promises;
 
 const app = express();
 
+const read = async () => {
+  const moviesPath = path.resolve(__dirname, "./movies.json");
+  try {
+    const movies = await fs.readFile(moviesPath, "utf-8");
+    const moviesList = JSON.parse(movies);
+    return moviesList;
+  } catch (e) {
+    console.error(e.message);
+  }
+};
+
 module.exports = {
-    app,
+  app,
 };
