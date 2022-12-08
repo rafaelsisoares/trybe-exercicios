f5e5b26795ef6fceb6f1267a281fe99f5c3d34ae
Rafael Soares <r.soares2015@outlook.com>
2022-12-08 16:03:47 -0300

    test: Conclusao do primeiro teste

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.3-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.3-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
@@ -3,6 +3,13 @@ const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
+const output = [
+    { id: 1, name: 'Mint Intense', brandId: 1 },
+    { id: 2, name: 'White Coconut', brandId: 1 },
+    { id: 3, name: 'Mon Chéri', brandId: 2 },
+    { id: 4, name: 'Mounds', brandId: 3 },
+  ];
+
 const { expect } = chai;
 
 describe('Usando o método GET em /chocolates', function() {
@@ -10,6 +17,8 @@ describe('Usando o método GET em /chocolates', function() {
         const response = await chai
             .request(app)
             .get('/chocolates');
+
         expect(response.status).to.be.equal(200);
+        expect(response.body.chocolates).to.deep.equal(output);
     });
 });
