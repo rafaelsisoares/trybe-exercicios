af705fdd21f0b5a3ef835c7c39584b2c77dc5d39
Rafael Soares <r.soares2015@outlook.com>
2022-12-08 15:55:16 -0300

    chore: Instalacao e configuracao do chai-http

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.3-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.3-node-js-testes-de-integracao/cacau-trybe/tests/integration/chocolates.test.js
@@ -1,10 +1,15 @@
 const chai = require('chai');
+const chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
 
 const { expect } = chai;
 
 describe('Usando o método GET em /chocolates', function() {
     it('Retorna a lista completa de chocolates', async function() {
-        response = await myRequest();
+        const response = await chai
+            .request(app)
+            .get('/chocolates');
         expect(response.status).to.be.equal(200);
     });
 });
