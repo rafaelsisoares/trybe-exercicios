8155fe70622e057023b65a3072b508d64e9f3fef
Rafael Soares <r.soares2015@outlook.com>
2022-12-08 20:17:40 -0300

    feat: Adicionada funcao que lista os chocolates por marca

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.3-node-js-testes-de-integracao/cacau-trybe/src/app.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.3-node-js-testes-de-integracao/cacau-trybe/src/app.js
@@ -11,8 +11,15 @@ app.get("/chocolates", async (_req, res) => {
 app.get("/chocolates/:id", async (req, res) => {
   const { id } = req.params;
   const chocolate = await cacauTrybe.getChocolateById(+id);
-  if (!chocolate) return res.status(404).json({ message: 'Chocolate not found' });
+  if (!chocolate) return res.status(404).json({ message: "Chocolate not found" });
   return res.status(200).json({ chocolate });
+});
+
+app.get("/chocolates/brands/:id", async (req, res) => {
+  const { id } = req.params;
+  const chocolates = await cacauTrybe.getChocolateByBrand(+id);
+  if (chocolates.length === 0) return res.status(404).json({ message: "Brand not found" });
+  return res.status(200).json({ chocolates });
 });
 
 module.exports = {
