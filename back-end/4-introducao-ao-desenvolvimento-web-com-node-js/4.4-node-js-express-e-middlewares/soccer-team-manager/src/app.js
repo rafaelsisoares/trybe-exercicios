14f4c800a792e1522c7a72f3d1569a140eea63f8
Rafael Soares <r.soares2015@outlook.com>
2022-12-12 16:03:27 -0300

    refactor: Refatoracao dos endpoints /teams/:id

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.4-node-js-express-e-middlewares/soccer-team-manager/src/app.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.4-node-js-express-e-middlewares/soccer-team-manager/src/app.js
@@ -46,28 +46,20 @@ app.get('/teams/:id', existingId, (req, res) => {
     res.status(201).json(team);
   });
   
-  app.put('/teams/:id', validateTeam, (req, res) => {
+  app.put('/teams/:id', validateTeam, existingId, (req, res) => {
     const id = Number(req.params.id);
     const team = teams.find((t) => t.id === id);
-    if (team) {
-      const index = teams.indexOf(team);
-      const updated = { id, ...req.body };
-      teams.splice(index, 1, updated);
-      res.status(201).json(updated);
-    } else {
-      res.sendStatus(400);
-    }
+    const index = teams.indexOf(team);
+    const updated = { id, ...req.body };
+    teams.splice(index, 1, updated);
+    res.status(201).json(updated);
   });
-  
-  // module.exports = app;
 
-app.delete('/teams/:id', (req, res) => {
+app.delete('/teams/:id', existingId, (req, res) => {
   const id = Number(req.params.id);
   const team = teams.find((t) => t.id === id);
-  if (team) {
-    const index = teams.indexOf(team);
-    teams.splice(index, 1);
-  }
+  const index = teams.indexOf(team);
+  teams.splice(index, 1);
   res.sendStatus(204);
 });
 
