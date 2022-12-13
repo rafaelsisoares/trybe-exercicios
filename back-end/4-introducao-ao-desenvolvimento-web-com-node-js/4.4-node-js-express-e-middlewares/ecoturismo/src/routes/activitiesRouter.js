2f33dcedc92e7e8c752454aa7207e13b087b22b6
Rafael Soares <r.soares2015@outlook.com>
2022-12-12 23:54:37 -0300

    feat: Criacao do middleware checkCreatedAt

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.4-node-js-express-e-middlewares/ecoturismo/src/routes/activitiesRouter.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.4-node-js-express-e-middlewares/ecoturismo/src/routes/activitiesRouter.js
@@ -4,16 +4,24 @@ const { checkName } = require('../middlewares/checkName');
 const checkPrice = require('../middlewares/checkPrice');
 const checkDescription = require('../middlewares/checkDescription');
 const { getAllActivities } = require('../utils/activitiesFunctions');
+const checkCreatedAt = require('../middlewares/checkCreatedAt');
 
 const router = Router();
 
 router.get('/activities', async (_req, res) => {
-    const activities = await getAllActivities();
-    res.status(200).json(activities);
+  const activities = await getAllActivities();
+  res.status(200).json(activities);
 });
 
-router.post('/activities', checkName, checkPrice, checkDescription, async (req, res) => {
+router.post(
+  '/activities',
+  checkName,
+  checkPrice,
+  checkDescription,
+  checkCreatedAt,
+  async (_req, res) => {
     res.status(201).json({ message: 'Atividade cadastrada com sucesso' });
-});
+  },
+);
 
 module.exports = router;
