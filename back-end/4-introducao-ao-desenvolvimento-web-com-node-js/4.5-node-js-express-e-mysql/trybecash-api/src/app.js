d6e6fc3f725f957717c1c3d4712c685d2dffeb8c
Rafael Soares <r.soares2015@outlook.com>
2022-12-13 16:22:40 -0300

    feat: Criacao do endpoint /people e conexao com o banco de dados

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.5-node-js-express-e-mysql/trybecash-api/src/app.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.5-node-js-express-e-mysql/trybecash-api/src/app.js
@@ -1,9 +1,12 @@
 // src/app.js
 
 const express = require('express');
+const peopleRouter = require('./routes/peopleRoutes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/people', peopleRouter);
 
 module.exports = app;
