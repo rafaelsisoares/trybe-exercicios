05070c9e6fc197a2e95ddc6a3bfd63adebb9b5b5
Rafael Soares <r.soares2015@outlook.com>
2022-12-12 23:32:40 -0300

    refactor: checkDescription refatorado

--- back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.4-node-js-express-e-middlewares/ecoturismo/src/middlewares/checkDescription.js
+++ back-end/4-introducao-ao-desenvolvimento-web-com-node-js/4.4-node-js-express-e-middlewares/ecoturismo/src/middlewares/checkDescription.js
@@ -1,17 +1,14 @@
+const checkDescriptionValues = (descriptionValue, res, value) => {
+    if (!descriptionValue) {
+        return res.status(400).json({ message: `O campo ${value} é obrigatório` });
+    }
+};
+
 module.exports = (req, res, next) => {
     const { description } = req.body;
-    if (!description) {
-        return res.status(400).json({ message: 'O campo description é obrigatório' });
-    }
-    if (!description.createdAt) {
-        return res.status(400).json({ message: 'O campo createdAt é obrigatório' });
-    }
-    if (!description.difficulty) {
-        return res.status(400).json({ message: 'O campo difficulty é obrigatório' });
-    }
-    if (!description.rating) {
-        return res.status(400).json({ message: 'O campo rating é obrigatório' });
-    }
-
-    next();
+    return checkDescriptionValues(description, res, 'description')
+    || checkDescriptionValues(description.createdAt, res, 'createdAt')
+    || checkDescriptionValues(description.difficulty, res, 'difficulty')
+    || checkDescriptionValues(description.rating, res, 'rating')
+    || next();
 };
