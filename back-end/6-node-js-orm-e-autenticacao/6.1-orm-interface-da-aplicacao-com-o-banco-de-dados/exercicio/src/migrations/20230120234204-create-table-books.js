80d569bfe5c8c3f3914c1501d60b1d728b94450e
Rafael Soares <r.soares2015@outlook.com>
2023-01-20 20:58:45 -0300

    feat: Criacao da migration create-table-books

---
+++ back-end/6-node-js-orm-e-autenticacao/6.1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/src/migrations/20230120234204-create-table-books.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('books', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      author: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      pageQuantity: {
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('books');
+  }
+};
