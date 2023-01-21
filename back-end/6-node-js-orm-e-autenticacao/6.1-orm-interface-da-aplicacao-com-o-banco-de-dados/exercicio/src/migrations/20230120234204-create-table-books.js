44824c0dc294f8b0245f053151dd4e9e93b2be7e
Rafael Soares <r.soares2015@outlook.com>
2023-01-20 21:36:33 -0300

    fix: Arrumando o nome da tabela

--- back-end/6-node-js-orm-e-autenticacao/6.1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/src/migrations/20230120234204-create-table-books.js
+++ back-end/6-node-js-orm-e-autenticacao/6.1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/src/migrations/20230120234204-create-table-books.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('books', {
+    await queryInterface.createTable('Books', {
       id: {
         allowNull: false,
         primaryKey: true,
