bb5b026db0a5731bb6679ed434ff84655adda305
Rafael Soares <r.soares2015@outlook.com>
2023-01-21 00:42:09 -0300

    feat: Criacao das rotas

--- back-end/6-node-js-orm-e-autenticacao/6.1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/src/index.js
+++ back-end/6-node-js-orm-e-autenticacao/6.1-orm-interface-da-aplicacao-com-o-banco-de-dados/exercicio/src/index.js
@@ -10,5 +10,7 @@ app.use(express.json());
 app.get('/books', booksController.getAll);
 app.get('/books/:id', booksController.getById);
 app.post('/books', booksController.create);
+app.put('/books/:id', booksController.update);
+app.delete('/books/:id', booksController.remove);
 
 app.listen(PORT, () => console.log(`Server Online at port ${PORT}`));
