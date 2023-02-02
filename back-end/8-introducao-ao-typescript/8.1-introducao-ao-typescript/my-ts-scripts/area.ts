bbbf9739cb5e94b7bb1ed607d0d847356e4b59bc
Rafael Soares <r.soares2015@outlook.com>
2023-02-01 22:57:43 -0300

    feat: Criacao de funcao de conversao de medidas de area

---
+++ back-end/8-introducao-ao-typescript/8.1-introducao-ao-typescript/my-ts-scripts/area.ts
@@ -0,0 +1,9 @@
+const areaUnits: string[] = ['km2', 'hm2', 'dam2', 'm2', 'dm2', 'cm2', 'mm2'];
+
+const convertArea = (value: number, fromUnit: string, toUnit: string) => {
+    const fromUnitIndex = areaUnits.indexOf(fromUnit);
+    const toUnitIndex = areaUnits.indexOf(toUnit);
+    const exponent = toUnitIndex - fromUnitIndex;
+
+    return value * Math.pow(10, exponent);
+};
