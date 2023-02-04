9d688f445430c9da6123b2d80f18ef2364bad842
Rafael Soares <r.soares2015@outlook.com>
2023-02-03 21:07:37 -0300

    feat: Criacao do endpoint DELETE /books/:id

--- back-end/8-introducao-ao-typescript/8.3-express-com-typescript/express-typescript/services/books.service.ts
+++ back-end/8-introducao-ao-typescript/8.3-express-com-typescript/express-typescript/services/books.service.ts
@@ -84,6 +84,15 @@ public async update(id: number, book: Book): Promise<void> {
 
     return this.model.update(id, book);
   }
+
+  public async remove(id: number): Promise<void> {
+    const bookFound = await this.model.getById(id);
+    if (!bookFound) {
+      throw new NotFoundError('Book not found!');
+    }
+
+    this.model.remove(id);
+  }
 }
 
 export default BookService;
