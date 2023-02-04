5e019e4583f3326b1d9aa1bd272e4899d837e97b
Rafael Soares <r.soares2015@outlook.com>
2023-02-04 17:45:00 -0300

    feat: Models funcionando

--- back-end/8-introducao-ao-typescript/8.2-tipagem-estatica-e-generics/typescript-with-sequelize/build/database/config/database.js
+++ back-end/8-introducao-ao-typescript/8.2-tipagem-estatica-e-generics/typescript-with-sequelize/build/database/config/database.js
@@ -1,23 +1,11 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+"use strict";
+require("dotenv/config");
+const config = {
+    username: process.env.DB_USER || 'root',
+    password: process.env.DB_PASS || 'root',
+    database: process.env.DB_NAME || 'books_api',
+    host: process.env.DB_HOST || 'localhost',
+    port: Number(process.env.DB_PORT) || 3306,
+    dialect: 'mysql',
+};
+module.exports = config;
