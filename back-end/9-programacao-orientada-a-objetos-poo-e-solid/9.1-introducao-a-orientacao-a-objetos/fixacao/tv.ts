fdda19541f98df1948c705f30be985cb3ce5828f
Rafael Soares <r.soares2015@outlook.com>
2023-02-09 16:28:40 -0300

    feat: Criacao dos metodos getter e setter

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/fixacao/tv.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/fixacao/tv.ts
@@ -1,24 +1,40 @@
 // Criação da classe Tv
 class Tv {
     // Atributos da classe
-    brand: string;
-    size: number;
-    resolution: string;
-    connections: string[];
-    connectedTo: string;
+    private _brand: string;
+    private _size: number;
+    private _resolution: string;
+    private _connections: string[];
+    private _connectedTo?: string;
 
     // Método construtor que é inicializado na criação de uma instância
     constructor(brand: string, size: number, resolution: string, connections: string[]) {
-        this.brand = brand;
-        this.size = size;
-        this.resolution = resolution;
-        this.connections = connections;
+        this._brand = brand;
+        this._size = size;
+        this._resolution = resolution;
+        this._connections = connections;
+    }
+
+    // Método getter
+    get connectionGetter(): string | undefined {
+        return this._connectedTo;
+    }
+
+    // Método setter
+    set connectionSetter(connection: string | undefined) {
+        if (!connection || this._connections.some((c) => c === connection)) {
+            this._connectedTo = connection;
+            console.log(this._connectedTo);
+        } else {
+            console.log('Sorry, connection unavailable');
+        }
+        ;
     }
 
     // Métodos da classe
     turnOn() {
-        console.log(`A TV ${this.brand} de ${this.size} polegadas, com resolução ${this.resolution}
-        que tem as conexões: ${this.connections} está ligada`);
+        console.log(`A TV ${this._brand} de ${this._size} polegadas, com resolução ${this._resolution}
+        que tem as conexões: ${this._connections} está ligada`);
     }
 }
 
