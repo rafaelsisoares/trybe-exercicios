3c2a01a72a579e9ceb3af2b5b2966f4eec6e0abc
Rafael Soares <r.soares2015@outlook.com>
2023-02-09 22:28:49 -0300

    feat: Criacao da classe Item

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/classes/Restaurant.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/classes/Restaurant.ts
@@ -5,3 +5,13 @@ class Client {
         this._name = name;
     }
 }
+
+class Item {
+    private _orderItem: string;
+    private _price: number;
+
+    constructor(orderItem: string, price: number) {
+        this._orderItem = orderItem;
+        this._price = price;
+    }
+}
