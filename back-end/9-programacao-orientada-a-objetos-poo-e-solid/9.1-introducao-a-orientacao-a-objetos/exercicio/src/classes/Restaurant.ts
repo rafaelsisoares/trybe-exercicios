4e5a352bd3eda7c1357515ac0944836bba16f3ed
Rafael Soares <r.soares2015@outlook.com>
2023-02-09 22:43:11 -0300

    feat: Criacao dos metodos de calculo total e total com desconto

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/classes/Restaurant.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/classes/Restaurant.ts
@@ -4,6 +4,10 @@ class Client {
     constructor(name: string) {
         this._name = name;
     }
+
+    get name(): string {
+        return this._name;
+    }
 }
 
 class Item {
@@ -14,16 +18,32 @@ class Item {
         this._orderItem = orderItem;
         this._price = price;
     }
+
+    get price(): number {
+        return this._price;
+    }
 }
 
 class Order {
     private _client: Client;
     private _items: Item[];
     private _payment: string;
+    private _discount: number;
 
-    constructor(client: Client, items: Item[], payment: string) {
+    constructor(client: Client, items: Item[], payment: string, discount: number) {
         this._client = client;
         this._items = items;
         this._payment = payment;
+        this._discount = discount;
+    }
+
+    total(): string {
+        const total = this._items.reduce((sum, item) => sum + item.price, 0);
+        return `${this._client.name}, o total do seu pedido é: ${total}, o método de pagamento escolhido foi ${this._payment}`;
+    }
+
+    totalWithDiscount(): string {
+        const total = this._items.reduce((sum, item) => sum + item.price, 0) * this._discount;
+        return `${this._client.name}, o total do seu pedido é: ${total}, o método de pagamento escolhido foi ${this._payment}`;
     }
 }
