7a69a56a76b0f33f0b282e69fb42935640fd8472
Rafael Soares <r.soares2015@outlook.com>
2023-02-10 22:37:29 -0300

    feat: Criacao da classe Person

---
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/classes/Person.ts
@@ -0,0 +1,42 @@
+class Person {
+    private _name: string;
+    private _birthDate: Date;
+
+    constructor(name: string, birthDate: Date) {
+        this._birthDate = birthDate;
+        this._name = name;
+    }
+
+    get name(): string {
+        return this._name;
+    };
+
+    set name(value: string) {
+        if (value.length < 3) {
+            throw new Error('Nome muito curto');
+        }
+
+        this._name = value;
+    }
+
+    get birthDate(): Date {
+        return this._birthDate;
+    }
+
+    set birthDate(value: Date) {
+        if (value.getTime() > new Date().getTime()) {
+            throw new Error('A data de nascimento não pode ser uma data futura');
+        }
+
+        if (this.getAge(value) >= 120) {
+            throw new Error('A pessoa deve ter no máximo 120 anos');
+        }
+    }
+
+    getAge(value: Date): number {
+        if (value.getMonth() >= new Date().getMonth()) {
+            return (new Date().getFullYear() - value.getFullYear()) - 1;
+        }
+        return new Date().getFullYear() - value.getFullYear();
+    }
+}
