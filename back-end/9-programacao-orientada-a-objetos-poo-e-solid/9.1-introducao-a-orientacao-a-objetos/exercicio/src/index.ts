6fffd0f6d4461cade89267a28edc1c8e5424d9ca
Rafael Soares <r.soares2015@outlook.com>
2023-02-11 21:48:30 -0300

    feat: Criando professores para as materias criadas anteriormente

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/index.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.1-introducao-a-orientacao-a-objetos/exercicio/src/index.ts
@@ -3,6 +3,7 @@ import { Client, Item, Order } from "./classes/Restaurant";
 import Data from "./classes/Date";
 import Person from "./classes/Person";
 import Subject from "./classes/Subject";
+import Teacher from "./classes/Teacher";
 
 const michael = new Person('Michael', new Date('1977-04-09'));
 const student = new Student(michael);
@@ -30,9 +31,11 @@ console.log('---------------------------------------------');
 
 const joanaBirthDay = new Date('2000-06-10');
 const johnBirthDay = new Date('1990-10-29');
+const lisaBirthDay = new Date('1999-12-25');
 
 const joana = new Person('Joana', joanaBirthDay);
 const john = new Person('John', johnBirthDay);
+const lisa = new Person('Lisa', lisaBirthDay);
 
 console.log(joana.name)
 console.log(joana.birthDate);
@@ -43,3 +46,11 @@ console.log(john.birthDate);
 const math = new Subject('Matemática');
 const history = new Subject('História');
 const philosophy = new Subject('Filosofia');
+
+const teacher1 = new Teacher(john, 2000, math);
+const teacher2 = new Teacher(joana, 2000, history);
+const teacher3 = new Teacher(lisa, 2000, philosophy);
+
+console.log(teacher1);
+console.log(teacher2);
+console.log(teacher3);
