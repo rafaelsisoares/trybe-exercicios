08b33bf65dd4632ef672a441327b78ca32020edb
Rafael Soares <r.soares2015@outlook.com>
2023-02-13 16:02:48 -0300

    feat: Criacao da classe abstrata Character e duas classes que herdam de Character

---
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.3-polimorfismo/fixacao/Character.ts
@@ -0,0 +1,39 @@
+abstract class Character {
+    abstract talk(): void;
+    abstract specialMove(): void;
+}
+
+class MeleeCharacter extends Character {
+    constructor(private _name: string, private _specialMoveName: string) {
+        super();
+    }
+    talk(): void {
+        console.log(`Hi I'm ${this._name}. I attack at close range`);
+    }
+
+    specialMove(): void {
+        console.log(`${this._name} used ${this._specialMoveName}`);
+    }
+}
+
+class LongRangeCharacter extends Character {
+    constructor(private _name: string, private _specialMoveName: string) {
+        super();
+    }
+
+    talk(): void {
+        console.log(`Hi I'm ${this._name}. I attack at long range`);
+    }
+
+    specialMove(): void {
+        console.log(`${this._name} used ${this._specialMoveName}`);
+    }
+}
+
+const yoshi = new MeleeCharacter('Yoshi', 'Super Dragon');
+const samus = new LongRangeCharacter('Samus', 'Zero Laser');
+
+yoshi.talk();
+yoshi.specialMove();
+samus.talk();
+samus.specialMove();
