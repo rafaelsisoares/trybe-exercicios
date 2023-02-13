a507689a8a77f12e5351910ea300b2f2001876e9
Rafael Soares <r.soares2015@outlook.com>
2023-02-13 19:49:14 -0300

    feat: Criacao da classe LocalDBModel e CharacterService

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.3-polimorfismo/fixacao/Character.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.3-polimorfismo/fixacao/Character.ts
@@ -1,3 +1,97 @@
+interface Character {
+    name: string;
+    specialMove: string;
+  }
+  
+  interface DbCharacter extends Character {
+    id: number;
+  }
+  
+  const db: DbCharacter[] = [];
+
+interface IModel {
+    insert(character: Character): DbCharacter;
+    getAll(): DbCharacter[];
+    getById(id: number): DbCharacter;
+    update(id: number, character: Character): DbCharacter;
+    remove(id: number): boolean;
+}
+
+class LocalDBModel implements IModel {
+    insert(character: Character) {
+        const lastId = db[db.length - 1].id;
+        const newCharacter = { id: lastId + 1, ...character };
+        db.push(newCharacter);
+        return newCharacter;
+    }
+    
+    getAll() {
+        return db;
+    }
+
+    getById(targetId: number) {
+        const character = db.find(({id}) => id === targetId);
+        if (!character) throw new Error('Personagem não encontrado');
+
+        return character;
+    }
+
+    update(id: number, character: Character) {
+        const index = db.findIndex((character) => character.id === id);
+        if (!character) throw new Error('Personagem não encontrado');
+
+        const editedCharacter = { id: db[index].id, ...character };
+        db[index] = editedCharacter;
+    }
+
+    remove(id: number) {
+        const index = db.findIndex((character) => character.id === id);
+        if (index < 0) return false;
+
+        db.splice(index, 1);
+        return true;
+    }
+}
+
+class CharacterService {
+    constructor(readonly db: LocalDBModel) {}
+
+    insert(character: Character) {
+        const newCharacter = this.db.insert(character);
+        return { status: 201, data: newCharacter };
+    }
+
+    getAll() {
+        const characters = this.db.getAll();
+        return { status: 200, data: characters };
+    }
+
+    getById(id: number) {
+        try {
+            const character = this.db.getById(id);
+            return { status: 200, data: character };
+        } catch (error) {
+            return { status: 404, data: error.message };
+        }
+    }
+
+    update(id: number, character: Character) {
+        try {
+            const editedCharacter = this.db.update(id, character);
+            return { status: 200, data: editedCharacter };
+        } catch (error) {
+            return { status: 404, data: error.message };
+        }
+    }
+
+    remove(id: number) {
+        const isRemoved = this.db.remove(id);
+        if (!isRemoved) return { status: 404, data: 'Personagem não encontrado' };
+
+        return { status: 204, data: '' };
+    }
+}
+
 abstract class Character {
     abstract talk(): void;
     abstract specialMove(): void;
