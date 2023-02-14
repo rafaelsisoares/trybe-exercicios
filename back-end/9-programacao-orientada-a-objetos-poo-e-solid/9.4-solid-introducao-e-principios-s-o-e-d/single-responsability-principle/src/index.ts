75ea816588c63dafc6ad65313d930bf4e38f080b
Rafael Soares <r.soares2015@outlook.com>
2023-02-14 19:29:43 -0300

    refactor: Refatoracao da funcao approvedStudents seguindo o principio aberto fechado

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.4-solid-introducao-e-principios-s-o-e-d/single-responsability-principle/src/index.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.4-solid-introducao-e-principios-s-o-e-d/single-responsability-principle/src/index.ts
@@ -1,5 +1,10 @@
 // ./src/index.ts
 
+type School = {
+  name: string,
+  approvalGrade: number,
+}
+
 type Discipline = {
     name: string;
     grade: number;
@@ -9,7 +14,7 @@ type Discipline = {
   type Student = {
     name: string;
     disciplines: Discipline[];
-    school?: string;
+    school: School;
   };
   
   /* Apoio para a função `getGradeLetter` */
@@ -41,8 +46,8 @@ type Discipline = {
   });
   
   /* "Determinar" */
-  const approvedStudents = ({ disciplines }: Student): boolean =>
-    disciplines.every(({ grade }) => grade > 0.7);
+  const approvedStudents = ({ disciplines, school }: Student): boolean =>
+    disciplines.every(({ grade }) => grade >= school.approvalGrade);
   
   /* "Atualizar" */
   const updateApprovalData = (student: Student): void => {
