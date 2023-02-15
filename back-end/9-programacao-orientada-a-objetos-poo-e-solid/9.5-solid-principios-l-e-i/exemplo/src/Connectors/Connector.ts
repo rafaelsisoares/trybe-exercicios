f2e4f7bdf64221321f15a61ba0316aabd035ce90
Rafael Soares <r.soares2015@outlook.com>
2023-02-15 16:54:27 -0300

    feat: Implementando o principio de segregacao de interfaces

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.5-solid-principios-l-e-i/exemplo/src/Connectors/Connector.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.5-solid-principios-l-e-i/exemplo/src/Connectors/Connector.ts
@@ -1,23 +1,25 @@
-// .src/Connectors/Connector.ts
+// ./src/Connectors/Connector.ts
 
-export interface Connector {
-    getCount(token: string): number | Promise<number>;
-  
-    incrementCount(token: string): void;
-    
-    closeConnection(): void;
-  
-    clearCount(token: string): void;
-  
-    firstCount(token: string): void;
-  }
-  
-  export interface ConnectorConstructor {
-    host: string;
-    port: number;
-    database?: string;
-    user?: string;
-    password?: string;
-  }
-  
-  export default Connector;
+export interface ReadOnlyConnector {
+  getCount(token: string): number | Promise<number>;
+
+  closeConnection(): void;
+}
+
+export interface Connector extends ReadOnlyConnector {
+  incrementCount(token: string): void;
+
+  clearCount(token: string): void;
+
+  firstCount(token: string): void;
+}
+
+export interface ConnectorConstructor {
+  host: string;
+  port: number;
+  database?: string;
+  user?: string;
+  password?: string;
+}
+
+export default Connector;
