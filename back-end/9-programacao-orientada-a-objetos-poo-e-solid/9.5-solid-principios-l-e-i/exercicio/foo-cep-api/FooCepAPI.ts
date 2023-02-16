9e421a62b7e29fcf9464402a10c144d84df26162
Rafael Soares <r.soares2015@outlook.com>
2023-02-15 21:38:31 -0300

    feat: Criacao de um mock para testes

--- back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.5-solid-principios-l-e-i/exercicio/foo-cep-api/FooCepAPI.ts
+++ back-end/9-programacao-orientada-a-objetos-poo-e-solid/9.5-solid-principios-l-e-i/exercicio/foo-cep-api/FooCepAPI.ts
@@ -1,5 +1,7 @@
 // ./FooCepAPI.ts
-class FooCepAPI {
+import IFooCepAPI from "./interfaces/IFooCepAPI";
+
+class FooCepAPI implements IFooCepAPI {
   async getAddressByCEP(cep: string, number: number): Promise<string> {
     return `O endereço para o "CEP:${cep}, n°:${number}" é "endereço foo"`;
   }
