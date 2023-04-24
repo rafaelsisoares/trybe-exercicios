33f29ce710f615a7ec96a7f60f4bf5ab1a6afc34
Rafael Soares <r.soares2015@outlook.com>
2023-04-24 20:27:17 -0300

    feat: Codigo que importa o codigo feito anteriormente para calcular quantas pessoas estavam no show

--- ciencia-da-computacao/1-introducao-a-python/1.1-aprendendo-python/fixacao/area.py
+++ ciencia-da-computacao/1-introducao-a-python/1.1-aprendendo-python/fixacao/area.py
@@ -1,21 +1,22 @@
 PI = 3.14
 
 
-def square (side):
-    '''Calculate area of square'''
+def square(side):
+    """Calculate area of square"""
     return side * side
 
 
-def rectangle (length, width):
-    '''Calculate area of rectangle'''
+def rectangle(length, width):
+    """Calculate area of rectangle"""
     return length * width
 
 
-def circle (radius):
-    '''Calculate area of circle'''
+def circle(radius):
+    """Calculate area of circle"""
     return PI * radius * radius
 
 
-print('Area do quadrado', square(10))
-print('Area do retangulo', rectangle(2, 2))
-print('Area do circulo', circle(3))
+if __name__ == '__main__':
+    print("Area do quadrado", square(10))
+    print("Area do retangulo", rectangle(2, 2))
+    print("Area do circulo", circle(3))
