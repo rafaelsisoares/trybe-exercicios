8df343c8ad53d97e787afb8eda0693f2c035c4a5
Rafael Soares <r.soares2015@outlook.com>
2023-05-04 17:04:05 -0300

    feat: Uso do padrao strategy

---
+++ ciencia-da-computacao/2-padroes-de-projeto/2.2-padroes-iterator-adapter-strategy/fixacao/strategy/strategies.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+
+
+class BancoStrategy(ABC):  # Interface
+    @classmethod
+    @abstractmethod
+    def debitar(cls):
+        raise NotImplementedError
+
+
+class ItauStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Itau (exemplo)
+        print("Débito realizado pelo Itau")
+
+
+class SantanderStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Santander (exemplo)
+        print("Santander, Débito efetuado!")
+
+
+class BradescoStrategy(BancoStrategy):
+    @classmethod
+    def debitar(cls, conta, valor):
+        # Codigos específico do Bradesco (exemplo)
+        print("Sucesso!")
+
+# ... métodos para todos os bancos
