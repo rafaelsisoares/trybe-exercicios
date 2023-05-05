b716970a7d382f857fa5c4d9c3e58a7b3ad43f9f
Rafael Soares <r.soares2015@outlook.com>
2023-05-04 22:49:41 -0300

    feat: Cria iterador para a classe Baralho

--- ciencia-da-computacao/2-padroes-de-projeto/2.2-padroes-iterator-adapter-strategy/exercicios/deck.py
+++ ciencia-da-computacao/2-padroes-de-projeto/2.2-padroes-iterator-adapter-strategy/exercicios/deck.py
@@ -1,3 +1,6 @@
+from collections.abc import Iterable, Iterator
+
+
 class Carta:
     def __init__(self, valor, naipe):
         self.valor = valor
@@ -13,8 +16,42 @@ class Baralho:
 
     def __init__(self):
         self._cartas = [
-            Carta(valor, naipe) for naipe in self.naipes for valor in self.valores
+            Carta(valor, naipe)
+            for naipe in self.naipes
+            for valor in self.valores
         ]
 
     def __len__(self):
         return len(self._cartas)
+
+
+class BaralhoIterable(Iterable):
+    def __init__(self, deck):
+        self.deck = deck
+
+    def __iter__(self):
+        return BaralhoIterator(self.deck)
+    
+    def __str__(self):
+        return f"{[card for card in self]}"
+
+
+class BaralhoIterator(Iterator):
+    def __init__(self, deck):
+        self.deck = deck
+        self._index = 0
+
+    def __next__(self):
+        try:
+            current_value = self.deck._cartas[self._index]
+        except IndexError:
+            raise StopIteration()
+        else:
+            self._index += 1
+            return current_value
+
+
+deck1 = Baralho()
+deckIter = BaralhoIterable(deck1)
+for card in deckIter:
+    print(card)
