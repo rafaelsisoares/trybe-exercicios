138b20dafc579b78c945599779543d52c832fb89
Rafael Soares <r.soares2015@outlook.com>
2023-05-05 00:36:49 -0300

    refactor: Refatora a classe que calcula os impostos e adiciona mais dois impostos

--- ciencia-da-computacao/2-padroes-de-projeto/2.2-padroes-iterator-adapter-strategy/exercicios/taxes.py
+++ ciencia-da-computacao/2-padroes-de-projeto/2.2-padroes-iterator-adapter-strategy/exercicios/taxes.py
@@ -1,20 +1,46 @@
+from abc import ABC, abstractmethod
+
+
+class CalculateTaxes(ABC):
+    @abstractmethod
+    def calculate_tax(cls):
+        raise NotImplementedError
+
+
+class CalculateIss(CalculateTaxes):
+    @classmethod
+    def calculate_tax(cls, value):
+        return value * 0.1
+
+
+class CalculateIcms(CalculateTaxes):
+    @classmethod
+    def calculate_tax(cls, value):
+        return value * 0.06
+
+
+class CalculatePis(CalculateTaxes):
+    @classmethod
+    def calculate_tax(cls, value):
+        return value * 0.0065
+
+
+class CalculateCofins(CalculateTaxes):
+    @classmethod
+    def calculate_tax(cls, value):
+        return value * 0.03
+
+
 class Orcamento:
     def __init__(self, valor):
         self.valor = valor
 
     def calcular_imposto(self, imposto):
-        if imposto == "ISS":
-            return self.__calcular_iss()
-        elif imposto == "ICMS":
-            return self.__calcular_icms()
-
-    def __calcular_iss(self):
-        return self.valor * 0.1
-
-    def __calcular_icms(self):
-        return self.valor * 0.06
+        return imposto.calculate_tax(self.valor)
 
 
 orcamento = Orcamento(1000)
-print(f"ISS: {orcamento.calcular_imposto('ISS')}")
-print(f"ICMS: {orcamento.calcular_imposto('ICMS')}")
+print(f"ISS: {orcamento.calcular_imposto(CalculateIss())}")
+print(f"ICMS: {orcamento.calcular_imposto(CalculateIcms())}")
+print(f"PIS: {orcamento.calcular_imposto(CalculatePis())}")
+print(f"COFINS: {orcamento.calcular_imposto(CalculateCofins())}")
