da044bb33a9ca5a3dad9bd8a53132c360524c74f
Rafael Soares <r.soares2015@outlook.com>
2023-05-11 22:00:06 -0300

    feat: Cria funcao que faz requisicoes como se fosse um navegador

--- ciencia-da-computacao/3-raspagem-de-dados/3.1-raspagem-de-dados/exercicios/src/more_scrape.py
+++ ciencia-da-computacao/3-raspagem-de-dados/3.1-raspagem-de-dados/exercicios/src/more_scrape.py
@@ -3,9 +3,8 @@ import requests
 url = 'https://scrapethissite.com/pages/advanced/?gotcha=headers'
 headers = {
     'accept': 'text/html',
-    'accept-encoding': 'gzip, deflate, br'
+    'user-agent': 'mozilla'
 }
 
 response = requests.get(url, headers=headers)
 assert "bot detected" not in response.text
-print(response.text)
