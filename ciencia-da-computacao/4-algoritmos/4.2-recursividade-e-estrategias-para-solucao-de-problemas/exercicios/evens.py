37c3980d9151b66397add99f841c9c2f7df3bc3e
Rafael Soares <r.soares2015@outlook.com>
2023-05-23 23:10:53 -0300

    feat: Implementa a funcao recursiva evens_recursive

--- ciencia-da-computacao/4-algoritmos/4.2-recursividade-e-estrategias-para-solucao-de-problemas/exercicios/evens.py
+++ ciencia-da-computacao/4-algoritmos/4.2-recursividade-e-estrategias-para-solucao-de-problemas/exercicios/evens.py
@@ -2,8 +2,17 @@ def evens(n: int) -> int:
     control = 1
     count = 0
     while control <= n:
-        if control % 2 == 0:
+        if control % 2 == 0 and n != 0:
             count += 1
         control += 1
 
     return count
+
+
+def evens_recursive(n: int) -> int:
+    if n == 1:
+        return 0
+    elif n % 2 == 0:
+        return 1 + evens_recursive(n - 1)
+    else:
+        return evens_recursive(n - 1)
