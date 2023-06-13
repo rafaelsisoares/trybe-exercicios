09e3e84df56cc5a341c051476b0e41ec1b5f94a1
Rafael Soares <r.soares2015@outlook.com>
2023-06-13 15:36:43 -0300

    feat: Populando os buckets

--- ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.1-hashmap-e-dict/fixacao/hashmap.py
+++ ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.1-hashmap-e-dict/fixacao/hashmap.py
@@ -22,3 +22,21 @@ class HashMap:
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
+    
+    def update_name(self, id_num, new_name):
+        address = self.get_address(id_num)
+        self._buckets[address].name = new_name
+
+
+if __name__ == '__main__':
+    employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+    registry = HashMap()
+    for id_num, name in employees:
+        employee = Employee(id_num, name)
+        registry.insert(employee)
+
+    print(registry.get_name(23))
+
+    print(registry.get_name(10))
+    registry.update_name(10, 'name30')
+    print(registry.get_name(10))
