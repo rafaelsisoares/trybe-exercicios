56bfb4a021e7ce6f02f2fe091ab874c1d9881bc3
Rafael Soares <r.soares2015@outlook.com>
2023-06-13 15:51:01 -0300

    fix: Resolvendo problema de colisao

--- ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.1-hashmap-e-dict/fixacao/hashmap.py
+++ ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.1-hashmap-e-dict/fixacao/hashmap.py
@@ -6,23 +6,26 @@ class Employee:
 
 class HashMap:
     def __init__(self):
-        self._buckets = [None for i in range(10)]
+        self._buckets = [[] for i in range(10)]
 
     def get_address(self, id_num):
         return id_num % 10
 
     def insert(self, employee):
         address = self.get_address(employee.id_num)
-        self._buckets[address] = employee
+        self._buckets[address].append(employee)
 
     def get_name(self, id_num):
         address = self.get_address(id_num)
-        return self._buckets[address].name
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
 
     def has(self, id_num):
         address = self.get_address(id_num)
         return self._buckets[address] is not None
-    
+
     def update_name(self, id_num, new_name):
         address = self.get_address(id_num)
         self._buckets[address].name = new_name
