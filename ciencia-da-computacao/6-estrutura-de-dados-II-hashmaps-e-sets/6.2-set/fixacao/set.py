21624d18b8a6a78d23367ebfe07fdc6383ffc5d7
Rafael Soares <r.soares2015@outlook.com>
2023-06-15 00:31:39 -0300

    feat: Finalizado exercicios de fixacao

--- ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.2-set/fixacao/set.py
+++ ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.2-set/fixacao/set.py
@@ -5,7 +5,7 @@ class Conjunto:
 
     def __str__(self):
         string = '{'
-        for index, is_in in enumerate(self._data):
+        for index, is_in in enumerate(self.data):
             if is_in:
                 string += str(index)
                 if index < self.last_element:
@@ -36,6 +36,25 @@ class Conjunto:
                 conjunto_intersection.add(index)
         return conjunto_intersection
 
+    def difference(self, conjunto_b):
+        conjunto_difference = Conjunto()
+        for index in range(1001):
+            if self.data[index] and not conjunto_b.data[index]:
+                conjunto_difference.add(index)
+        return conjunto_difference
+
+    def issubset(self, conjunto_b):
+        for index in range(1001):
+            if self.data[index] and not conjunto_b.data[index]:
+                return False
+        return True
+
+    def issuperset(self, conjunto_b):
+        for index in range(1001):
+            if conjunto_b.data[index] and not self.data[index]:
+                return False
+            return True
+
 
 if __name__ == '__main__':
     values = [0, 10, 100, 1000]
@@ -44,3 +63,28 @@ if __name__ == '__main__':
         conjunto.add(value)
 
     print(100 in conjunto)
+
+    estudantes = [1, 2, 3, 4, 5, 6, 7]
+    lista1_entregues = [1, 4, 7, 3]
+    lista2_entregues = [3, 1, 6]
+
+    students = Conjunto()
+    project1 = Conjunto()
+    project2 = Conjunto()
+    for estudante in estudantes:
+        students.add(estudante)
+    for student in lista1_entregues:
+        project1.add(student)
+    for student in lista2_entregues:
+        project2.add(student)
+
+    print(f"""Estudantes que não entregaram o primeiro projeto:
+            {students.difference(project1)}""")
+    print(f"""Estudantes que entregaram os dois projetos:
+            {project1.intersection(project2)}""")
+    print(f"""Estudantes que entregaram os dois projetos:
+            {project1.intersection(project2)}""")
+    print(f"""Estudantes que entregaram pelo menos um projeto:
+            {project1.union(project2)}""")
+    print(f"""Estudantes que não entregaram nenhum projeto:
+            {students.difference(project1.union(project2))}""")
