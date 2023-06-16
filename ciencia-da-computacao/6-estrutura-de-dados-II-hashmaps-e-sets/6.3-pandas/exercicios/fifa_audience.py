4d1a16307697b915b436c52ce266199bbb95ab99
Rafael Soares <r.soares2015@outlook.com>
2023-06-16 00:36:01 -0300

    feat: Imprime os paises com o population_share > 2

--- ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.3-pandas/exercicios/fifa_audience.py
+++ ciencia-da-computacao/6-estrutura-de-dados-II-hashmaps-e-sets/6.3-pandas/exercicios/fifa_audience.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-data = pd.read_csv('data/fifa_audience.csv')
-df = pd.DataFrame(data)
-print(df)
+
+def create_data_frame(path):
+    data = pd.read_csv(path)
+    df = pd.DataFrame(data)
+    return df
+
+
+if __name__ == '__main__':
+    df = create_data_frame('data/fifa_audience.csv')
+    print(df)
+    print(df['country'].loc[df['population_share'] > 2])
